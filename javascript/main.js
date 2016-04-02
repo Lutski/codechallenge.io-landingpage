9dc1df13c84a8729cb33d8aed42822b5428e0f1a
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-04-02 18:38:06 +0200

    Added footer section + its javascript functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,21 @@
 $(document).ready(function() {
+  getDay();
 });
+
+function getDay() {
+  var currentDate = new Date();
+  var dayOfTheWeek = currentDate.getDay();
+
+  var weekDays = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+  }
+
+  var getCurrentDay = weekDays[dayOfTheWeek];
+  $('#footer h1 span').text(getCurrentDay);
+}
