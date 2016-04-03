55d0ede6e88d9e7d46ec3834d3a81d56cadf0ef3
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-04-03 14:03:56 +0200

    Minor JS fix in the function getDay()

--- javascript/main.js
+++ javascript/main.js
@@ -7,13 +7,13 @@ function getDay() {
   var dayOfTheWeek = currentDate.getDay();
 
   var weekDays = {
+    0: "Sunday",
     1: "Monday",
     2: "Tuesday",
     3: "Wednesday",
-    4: "Thursday",
+    4: "Thursdau",
     5: "Friday",
-    6: "Saturday",
-    7: "Sunday"
+    6: "Saturday"
   }
 
   var getCurrentDay = weekDays[dayOfTheWeek];
